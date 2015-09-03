30c1ca23fed8e998296624b0f923f0c983aeb3ac
Ivonei Junior <ivoneijr@gmail.com>
2015-09-03 12:01:55 -0300

    initial user model and controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+User.create(username: 'ivoneijr', email: 'ivoneijr@gmail.com', password: 'ivoneijr')
