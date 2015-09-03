ddaa625c8861878eeee39199af5a90367dfe7346
Ivonei Junior <ivoneijr@gmail.com>
2015-09-03 17:28:00 -0300

    application_controller token authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,25 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
+
+	include ActionController::HttpAuthentication::Token::ControllerMethods
+	before_action :authenticate
+
+	protected
+
+    def authenticate
+      authenticate_token || render_unauthorized
+    end
+
+    def authenticate_token
+      authenticate_with_http_token do |token, options|
+        @authenticated = User.find_by(token: token)
+      end
+    end
+
+    def render_unauthorized
+      self.headers['WWW-Authenticate'] = 'Token realm="API"'
+      render json: {message: 'Bad credentials'}, status: :unauthorized
+    end
+
 end
