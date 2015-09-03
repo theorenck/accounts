c7b38f1092d074abe80fc4183fbbd86f80e41dee
Ivonei Junior <ivoneijr@gmail.com>
2015-09-03 14:34:56 -0300

    update code

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,3 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-
-
-User.create(username: 'ivoneijr', email: 'ivoneijr@gmail.com', password: 'ivoneijr')
