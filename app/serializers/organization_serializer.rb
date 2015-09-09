3516ec230534908f61ab1183a30e6ad6750cfb40
Ivonei Junior <ivoneijr@gmail.com>
2015-09-09 18:25:47 -0300

    Change service_instances relationships in organization_serializer

--- app/serializers/organization_serializer.rb
+++ app/serializers/organization_serializer.rb
@@ -1,6 +1,6 @@
 class OrganizationSerializer < ActiveModel::Serializer
   attributes :id, :name, :created_at, :updated_at
   has_one :owner
-  has_many :services
+  has_many :service_instances
   has_many :members
 end
