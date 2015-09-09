6cf15c39d3ac7936073ee871fc114144a8ef1831
unknown <carlos.renck@PROD-07.zeta.local>
2015-09-09 18:14:58 -0300

    associando admin a organização zeta no seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 
 admin = User.create(username: 'admin', email: 'carlos.renck@zeta.com', password: 'admin', token:'3c897fa3098770eef3ed71e314d4c2b1')
-zeta = Organization.create(name: 'Zeta Informatica', owner_id: admin.id)
-
+zeta = Organization.create(name: 'Zeta Informatica', owner: admin)
+Membership.create(organization:zeta, user:admin)
 
 middleware = ServiceType.create(code: 'middleware', name: 'middleware', description: 'Zeta Middleware')
 v1 = Service.create(version: 'v1', type_id: middleware.id)
