af5c3fd28892b7e16aac42909341f2f5c16becda
Ivonei Junior <ivoneijr@gmail.com>
2015-10-02 18:28:47 -0300

    back to sql on migration

--- db/migrate/20150930201712_create_accounts.rb
+++ db/migrate/20150930201712_create_accounts.rb
@@ -3,11 +3,11 @@ class CreateAccounts < ActiveRecord::Migration
   execute <<-SQL
   CREATE OR REPLACE VIEW accounts AS
     SELECT
-     users.username 	       AS user_username,
-     users.email 	           AS user_email,
-     organizations.name 	   AS organization_name,
-     memberships.authorized  AS membership_authorized,
-     applications.name 	     AS application_name,
+     users.username 	         AS user_username,
+     users.email 	             AS user_email,
+     organizations.name 	     AS organization_name,
+     memberships.authorized    AS membership_authorized,
+     applications.name 	       AS application_name,
      applications.redirect_uri AS application_redirect_uri,
      applications.version      AS application_version,
      authorizations.revoked_at AS authorization_revoked_at
